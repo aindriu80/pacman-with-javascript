06aee0efabc9e82acd37682e29a444ab826452d2
aindriu80 <aindriu80@gmail.com>
2020-09-21 16:34:15 +0100

    Scaffolding

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,25 @@
-console.log('Working!');
+import { LEVEL, OBJECT_TYPE } from "./setup";
+
+// Dom Elements
+const gameGrid = document.querySelector("#gmae");
+const scoreTable = document.querySelector("#score");
+const startButton = document.querySelector("#start-button");
+
+// Game constants
+const POWER_PILL_TIME = 10000; // milliseconds
+const GLOBAL_SPEED = 80; // milliseconds
+
+// Initial Setup
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
+function gameOver(pacman, grid) {}
+
+function checkCollision(pacman, ghosts) {}
+
+function gameLoop(pacman, ghosts) {}
+
+function startGame() {}
