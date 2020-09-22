af166654447a8f174657baada197330dfcb0946e
aindriu80 <aindriu80@gmail.com>
2020-09-22 21:48:05 +0100

    Getting Pacman on the board

--- resources/js/GameBoard.js
+++ resources/js/GameBoard.js
@@ -1,55 +1,55 @@
-import { GRID_SIZE, CELL_SIZE, OBJECT_TYPE, CLASS_LIST } from "./setup";
+import { GRID_SIZE, CELL_SIZE, OBJECT_TYPE, CLASS_LIST } from './setup'
 
 class GameBoard {
   constructor(DOMGrid) {
-    this.dotCount = 0;
-    this.grid = [];
-    this.DOMGrid = DOMGrid;
+    this.dotCount = 0
+    this.grid = []
+    this.DOMGrid = DOMGrid
   }
 
   showGameStatus(gameWin) {
-    const div = document.createElement("div");
-    div.classList.add("game-status");
-    div.innnerHTML = `${gameWin ? "WIN!" : "Game Over!"}`;
-    this.DOMGrid.appendChild(div);
+    const div = document.createElement('div')
+    div.classList.add('game-status')
+    div.innnerHTML = `${gameWin ? 'WIN!' : 'Game Over!'}`
+    this.DOMGrid.appendChild(div)
   }
   createGrid(level) {
-    this.dotCount = 0;
-    this.grid = [];
-    this.DOMGrid.innerHTML = "";
-    this.DOMGrid.style.cssText = `grid-template-columns: repeat(${GRID_SIZE}, ${CELL_SIZE}px)`;
+    this.dotCount = 0
+    this.grid = []
+    this.DOMGrid.innerHTML = ''
+    this.DOMGrid.style.cssText = `grid-template-columns: repeat(${GRID_SIZE}, ${CELL_SIZE}px)`
 
     level.forEach((square) => {
-      const div = document.createElement("div");
-      div.classList.add("square", CLASS_LIST[square]);
-      div.style.cssText = `width:${CELL_SIZE}px; height: ${CELL_SIZE}px;`;
-      this.DOMGrid.appendChild(div);
-      this.grid.push(div);
+      const div = document.createElement('div')
+      div.classList.add('square', CLASS_LIST[square])
+      div.style.cssText = `width:${CELL_SIZE}px; height: ${CELL_SIZE}px;`
+      this.DOMGrid.appendChild(div)
+      this.grid.push(div)
 
-      if (CLASS_LIST[square] === OBJECT_TYPE.DOT) this.dotCount++;
-    });
+      if (CLASS_LIST[square] === OBJECT_TYPE.DOT) this.dotCount++
+    })
   }
 
   addObject(pos, classesApply) {
-    this.grid[pos].classList.add(...classesApply);
+    this.grid[pos].classList.add(...classesApply)
   }
 
-  removeObj(pos, classesApply) {
-    this.grid[pos].classList.remove(...classesApply);
+  removeObject(pos, classesApply) {
+    this.grid[pos].classList.remove(...classesApply)
   }
 
-  objectExist(pos, object) {
-    return this.grid[pos].classList.contains(object);
+  objectExist = (pos, object) => {
+    return this.grid[pos].classList.contains(object)
   }
 
   rotateDiv(pos, deg) {
-    this.grid[pos].style.transform = `rotate(${deg}deg)`;
+    this.grid[pos].style.transform = `rotate(${deg}deg)`
   }
 
   static createGameBoard(DOMGrid, level) {
-    const board = new this(DOMGrid);
-    board.createGrid(level);
-    return board;
+    const board = new this(DOMGrid)
+    board.createGrid(level)
+    return board
   }
 }
-export default GameBoard;
+export default GameBoard
--- resources/js/index.js
+++ resources/js/index.js
@@ -1,23 +1,23 @@
-import { LEVEL, OBJECT_TYPE } from "./setup";
+import { LEVEL, OBJECT_TYPE } from './setup'
 // Classes
-import GameBoard from "./GameBoard";
-
+import GameBoard from './GameBoard'
+import Pacman from './Pacman'
 // Dom Elements
-const gameGrid = document.querySelector("#game");
-const scoreTable = document.querySelector("#score");
-const startButton = document.querySelector("#start-button");
+const gameGrid = document.querySelector('#game')
+const scoreTable = document.querySelector('#score')
+const startButton = document.querySelector('#start-button')
 
 // Game constants
-const POWER_PILL_TIME = 10000; // milliseconds
-const GLOBAL_SPEED = 80; // milliseconds
-const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL);
+const POWER_PILL_TIME = 10000 // milliseconds
+const GLOBAL_SPEED = 80 // milliseconds
+const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL)
 
 // Initial Setup
-let score = 0;
-let timer = null;
-let gameWin = false;
-let powerPillActive = false;
-let powerPillTimer = null;
+let score = 0
+let timer = null
+let gameWin = false
+let powerPillActive = false
+let powerPillTimer = null
 
 function gameOver(pacman, grid) {}
 
@@ -25,4 +25,21 @@ function checkCollision(pacman, ghosts) {}
 
 function gameLoop(pacman, ghosts) {}
 
-function startGame() {}
+function startGame() {
+  gameWin = false
+  powerPillActive = false
+  score = 0
+
+  startButton.classList.add('hide')
+
+  gameBoard.createGrid(LEVEL)
+
+  const pacman = new Pacman(2, 287)
+  gameBoard.addObject(287, [OBJECT_TYPE.PACMAN])
+  document.addEventListener('keydown', (e) =>
+    pacman.handleKeyInput(e, gameBoard.objectExist)
+  )
+}
+
+// Initialize Game
+startButton.addEventListener('click', startGame)
