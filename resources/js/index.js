b4234420bb67e81afd323cde133dbaf187840a2b
aindriu80 <aindriu80@gmail.com>
2020-09-26 13:59:02 +0100

    Eating Dots, Powerpill and Displaying Score

--- resources/js/index.js
+++ resources/js/index.js
@@ -59,6 +59,38 @@ function gameLoop(pacman, ghosts) {
 
   ghosts.forEach((ghost) => gameBoard.moveCharacter(ghost))
   checkCollision(pacman, ghosts)
+
+  // Check if Pacman eats a dot
+  if (gameBoard.objectExist(pacman.pos, OBJECT_TYPE.DOT)) {
+    gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.DOT])
+    gameBoard.dotCount--
+    score += 10
+  }
+  // Check if Pacman eats a powerpill
+  if (gameBoard.objectExist(pacman.pos, OBJECT_TYPE.PILL)) {
+    gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.PILL])
+
+    pacman.powerPill = true
+    score += 50
+
+    clearTimeout(powerPillTimer)
+    powerPillTimer = setTimeout(
+      () => (pacman.powerPill = false),
+      POWER_PILL_TIME
+    )
+  }
+  // Change ghost scare mode depending on Powerpill
+  if (pacman.powerPill !== powerPillActive) {
+    powerPillActive = pacman.powerPill
+    ghosts.forEach((ghost) => (ghost.isScared = pacman.powerPill))
+  }
+  // Check if all dots have been eaten
+  if (gameBoard.dotCount === 0) {
+    gameWin = true
+    gameOver(pacman, ghost)
+  }
+  // Show the score in the scoreboard
+  scoreTable.innerHTML = score
 }
 
 function startGame() {
